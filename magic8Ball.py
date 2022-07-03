336edddf1ac391e6a07d7019bd38272482a1a0be
kdasari07 <106094722+kdasari07@users.noreply.github.com>
2022-07-03 15:12:08 +0530

    Update magic8Ball.py

--- magic8Ball.py
+++ magic8Ball.py
@@ -1,26 +1,19 @@
 from random import randint
+responses = ["Yes","No","Probably","Maybe","I Don't Think So","Perhaps","It's Possible",
+             "Yes, I think so"]
+print("Welcome to the Magic 8 ball")
 
-responses = ["Yes", "No", "Probably", "Maybe", "I don't think so", "Perhaps",
-             "It's possible", "Yes, I think so"]
-
-print("Welcome to the Magic 8 ball!")
-
-while True:
-    question = input("Ask me a question: ")
-    print responses[randint(0, 7)]
-
-    while True:
-        play = input("Would you like to play again? (Y/N) ")
-        print ""
-
-        if play == "Y" or play == "y":
-            continue
-
-        elif play == "N" or play == "n":
-            print("Goodbye!")
-            break
-
-        else:
-            print("I didn't quite catch that.")
-
-    break
+def eight_ball():
+  question = input("Ask a question")
+  print(responses[randint(0,7)])
+  
+  while True:
+    play = input("Would you like to play again?")  #Y,y for yes and N,n for no
+    
+    if play=="Y" or play=="y":
+      eight_ball()
+     elif play=="N" or play=="n":
+      break
+     else:
+      print("I didn't quite catch that")
+eight_ball()
